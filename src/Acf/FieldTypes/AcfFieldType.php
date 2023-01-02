63b8b675a3c447c72e09e21ba41f45d18f9ba477
Freekiehsoes <freekattema@live.nl>
2023-01-02 11:28:29 +0100

    test

--- src/Acf/FieldTypes/AcfFieldType.php
+++ src/Acf/FieldTypes/AcfFieldType.php
@@ -3,76 +3,65 @@
 namespace Freekattema\Wp\Acf\FieldTypes;
 
 abstract class AcfFieldType {
-	/** @var string  */
-	private $label;
-	/** @var string  */
-	private $name;
-	/** @var int  */
-	private $width = 100;
-	/** @var bool */
-	private $required = false;
+    /** @var string  */
+    private $label;
+    /** @var string  */
+    private $name;
+    /** @var int  */
+    private $width = 100;
 
-	protected array $additional = [];
+    private function __construct(string $label, string $name) {
+        $this->label = $label;
+        $this->name = $name;
+    }
 
-	private function __construct(string $label, string $name) {
-		$this->label = $label;
-		$this->name = $name;
-	}
+    public static function create(string $label, string $name)
+    {
+        return new static($label, strtolower($name));
+    }
 
-	public static function create(string $label, string $name)
-	{
-		return new static($label, strtolower($name));
-	}
+    public function to_array(): array
+    {
+        $DEFAULT = [
+            'key' => $this->get_key(),
+            'label' => $this->label,
+            'name' => $this->name,
+            'type' => $this->get_type(),
+            'wrapper' => [
+                'width' => $this->width,
+            ]
+        ];
 
-	public function to_array(): array
-	{
-		$DEFAULT = [
-			'key' => $this->get_key(),
-			'label' => $this->label,
-			'name' => $this->name,
-			'type' => $this->get_type(),
-			'required' => $this->required ? 1 : 0,
-			'wrapper' => [
-				'width' => $this->width,
-			]
-		];
+        return array_merge($DEFAULT, $this->get_additional());
+    }
 
-		return array_merge($DEFAULT, $this->get_additional(), $this->additional);
-	}
+    public function width(int $width): AcfFieldType
+    {
+        // cap between 0 and 100
+        $width = max(0, min(100, $width));
+        $this->width = $width;
+        return $this;
+    }
 
-	public function width(int $width): AcfFieldType
-	{
-		// cap between 0 and 100
-		$width = max(0, min(100, $width));
-		$this->width = $width;
-		return $this;
-	}
+    public function get_key(): string
+    {
+        $name = strtolower($this->name);
 
-	public function required($required = true): AcfFieldType
-	{
-		$this->required = $required;
-		return $this;
-	}
+        $random_key = substr(md5($name), 0, 6);
 
-	public function get_key(): string
-	{
-		$name = strtolower($this->name);
+        return 'zigzag_field_' . $random_key;
+    }
 
-		$random_key = substr(md5($name), 0, 6);
+    abstract function get_additional(): array;
+    abstract function get_type(): string;
 
-		return 'zigzag_field_' . $random_key;
-	}
+    public function get_label()
+    {
+        return $this->label;
+    }
 
-	abstract function get_additional(): array;
-	abstract function get_type(): string;
-
-	public function get_label()
-	{
-		return $this->label;
-	}
-
-	public function get_name()
-	{
-		return $this->name;
-	}
+    public function get_name()
+    {
+        return $this->name;
+    }
 }
---
+++ src/Acf/FieldTypes/AcfPostObject.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Freekattema\Wp\Acf\FieldTypes;
+
+use Freekattema\Wp\Acf\Traits\HasOptions;
+
+class AcfPostObject extends AcfFieldType {
+	use HasOptions;
+	function get_additional(): array {
+		return $this->merge_with_options([
+			'post_type' => ['post'],
+			'taxonomy' => [],
+			'allow_null' => 0,
+			'multiple' => 0,
+			'return_format' => 'object',
+		]);
+	}
+
+	/**
+	 * @param string|string[] $post_type
+	 *
+	 * @return AcfPostObject
+	 */
+	public function post_type($post_type): AcfPostObject {
+		$option = $this->get_options()['post_type'] ?? [];
+		if (is_array($post_type)) {
+			$option = array_merge($option, $post_type);
+		} else {
+			$option[] = $post_type;
+		}
+		return $this->set_option('post_type', $option);
+	}
+
+	/**
+	 * @param string|string[] $taxonomy
+	 *
+	 */
+	public function taxonomy($taxonomy): AcfPostObject {
+		$option = $this->get_options()['taxonomy'] ?? [];
+		if (is_array($taxonomy)) {
+			$option = array_merge($option, $taxonomy);
+		} else {
+			$option[] = $taxonomy;
+		}
+		return $this->set_option('taxonomy', $option);
+	}
+
+	/**
+	 * @param bool $allow_null
+	 *
+	 * @return AcfPostObject
+	 */
+	public function allow_null(bool $allow_null = true): AcfPostObject {
+		return $this->set_option('allow_null', $allow_null ? 1 : 0);
+	}
+
+	/**
+	 * @param bool $multiple
+	 *
+	 * @return AcfPostObject
+	 */
+	public function multiple(bool $multiple = true): AcfPostObject {
+		return $this->set_option('multiple', $multiple ? 1 : 0);
+	}
+
+	function get_type(): string {
+		return 'post_object';
+	}
+}
