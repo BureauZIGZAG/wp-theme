0df16d4f565401e0e3003fc216ac19c245e20566
Freekiehsoes <freekattema@live.nl>
2023-03-21 13:05:35 +0100

    fix for child themes

--- src/Components/ComponentAssetsLoader.php
+++ src/Components/ComponentAssetsLoader.php
@@ -10,9 +10,9 @@ final class ComponentAssetsLoader
 
     public static function attach(string $component)
     {
-        $cssFile = '/dist/exports/' . $component . '.css';
-        if (file_exists(get_template_directory() . $cssFile)) {
-            Buffer::add_css(get_template_directory_uri() . $cssFile);
-        }
+	    $cssFile = '/dist/exports/' . $component . '.css';
+	    if ( file_exists( get_stylesheet_directory() . $cssFile ) ) {
+		    Buffer::add_css( get_stylesheet_directory_uri() . $cssFile );
+	    }
     }
 }
